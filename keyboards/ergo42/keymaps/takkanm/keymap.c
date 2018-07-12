b62b46c18a2b3b1be992b2dda5b5775fa950afee
takkanm <takkanm@gmail.com>
2018-07-12 14:54:35 +0900

    set meta layout

--- keyboards/ergo42/keymaps/takkanm/keymap.c
+++ keyboards/ergo42/keymaps/takkanm/keymap.c
@@ -27,8 +27,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT( \
     KC_ESC,   KC_TAB,          KC_Q,        KC_W,    KC_E,   KC_R,     KC_T,       KC_Y,   KC_U,   KC_I,    KC_O,          KC_P,              KC_BSLS,        KC_BSPC, \
     KC_DELT,  CTL_T(KC_BSPC),  KC_A,        KC_S,    KC_D,   KC_F,     KC_G,       KC_H,   KC_J,   KC_K,    KC_L,          LT(SYMB, KC_SCLN), GUI_T(KC_QUOT), KC_ENT, \
-    MO(SYMB), KC_LSFT,         CTL_T(KC_Z), KC_X,    KC_C,   KC_V,     KC_B,       KC_N,   KC_M,   KC_COMM, KC_DOT,        CTL_T(KC_SLSH),    KC_RSFT,        KC_RSFT, \
-    MO(META), LT(SYMB,KC_GRV), KC_QUOT,     KC_LALT, KC_BSPC, KC_LGUI, KC_SPC,     KC_SPC, KC_RGUI, KC_ENT,  CTL_T(KC_ESC), KC_LBRC,          KC_RBRC,        KC_FN1 \
+    MO(SYMB), KC_LSFT,         CTL_T(KC_Z), KC_X,    KC_C,   KC_V,     KC_B,       KC_N,   KC_M,   KC_COMM, KC_DOT,        CTL_T(KC_SLSH),    KC_RSFT,        MO(META), \
+    MO(META), LT(SYMB,KC_GRV), KC_QUOT,     KC_LALT, KC_BSPC, KC_SPC, KC_LGUI,     KC_RGUI, KC_SPC, KC_ENT,  CTL_T(KC_ESC), KC_LBRC,          KC_RBRC,        KC_FN1 \
   ),
 
   /* META
@@ -43,9 +43,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [META] = LAYOUT( \
-    RESET,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,    KC_RBRC,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-    _______, KC_F1,   XXXXXXX, KC_MHEN, KC_HENK, XXXXXXX, XXXXXXX,    KC_LEFT,    KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, _______, \
-    _______, KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   S(KC_LBRC), S(KC_RBRC), KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
+    RESET,   KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,   KC_BSPC, \
+    _______, KC_F1,   KC_F2, KC_MHEN, KC_HENK, XXXXXXX, XXXXXXX,    KC_LEFT,    KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, _______, \
+    _______, KC_LSFT,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, _______, _______,\
     _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX \
   ),
 
