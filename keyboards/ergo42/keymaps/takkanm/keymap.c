98fd403ad388bd958ddd5fb9e7b127bcd2a0a314
takkanm <takkanm@gmail.com>
2018-07-17 17:27:07 +0900

    fix right meta

--- keyboards/ergo42/keymaps/takkanm/keymap.c
+++ keyboards/ergo42/keymaps/takkanm/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RESET,   KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_BSPC, \
     _______, KC_F1,   KC_F2,   KC_MHEN, KC_HENK, XXXXXXX, XXXXXXX,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, _______, \
     _______, KC_LSFT, KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,      KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,\
-    _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX \
+    _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, XXXXXXX, XXXXXXX, _______ \
   ),
 
   /* SYMB
