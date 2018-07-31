bcdacb70aa99bd4df598d100ce8e0c6a8c5f6c89
takkanm <takkanm@gmail.com>
2018-07-31 11:30:41 +0900

    set first keymap

--- keyboards/helix/rev2/keymaps/takkanm/keymap.c
+++ keyboards/helix/rev2/keymaps/takkanm/keymap.c
@@ -74,11 +74,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT( \
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-      ADJUST,  KC_ESC,  KC_LALT, KC_LGUI, EISU,    LOWER,   KC_SPC,  KC_SPC,  RAISE,   KANA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      KC_EQL,  KC_1,     KC_2,   KC_3,   KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
+      KC_TAB,  KC_Q,     KC_W,   KC_E,   KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
+      KC_LCTL, KC_A,     KC_S,   KC_D,   KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+      KC_LSFT, KC_Z,     KC_X,   KC_C,   KC_V,    KC_B,   KC_ESC,   KC_ESC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT , \
+      KC_GRV,  KC_QUOT,  ADJUST, LOWER,  KC_LALT, KC_SPC, KC_LGUI,  KC_RGUI, KC_ENT,  KC_SPC,  RAISE, KC_LBRC, KC_RBRC,   KC_BSPC \
       ),
 
   /* Colemak
