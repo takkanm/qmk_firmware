2045a27859ca594726a70dcba0c46978f164daf3
takkanm <takkanm@gmail.com>
2018-08-20 09:42:18 +0900

    synchronize other keybords

--- keyboards/ergo42/keymaps/takkanm/keymap.c
+++ keyboards/ergo42/keymaps/takkanm/keymap.c
@@ -6,6 +6,9 @@ extern keymap_config_t keymap_config;
 #define META 1
 #define SYMB 2
 #define GAME 3
+#define RAISE MO(META)
+#define LOWER MO(SYMB)
+#define ADJUST MO(GAME)
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
@@ -25,10 +28,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [BASE] = LAYOUT( \
-    KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,   S(KC_4),   S(KC_5),  KC_Y,   KC_U,   KC_I,     KC_O,    KC_P,    KC_BSLS, \
-    KC_LCTL,  KC_A,    KC_S,   KC_D,    KC_F,    KC_G,   S(KC_9),   S(KC_0),  KC_H,   KC_J,   KC_K,     KC_L,    KC_SCLN, KC_QUOT, \
-    KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,   MO(SYMB),  MO(META), KC_N,   KC_M,   KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT , \
-    MO(META), KC_GRV,  GAME,   KC_LALT, KC_SPC,  KC_ESC, KC_LGUI,   KC_RGUI,  KC_ENT, KC_SPC, MO(META), KC_LBRC, KC_RBRC, MO(SYMB)\
+    KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,    S(KC_4),   S(KC_5),  KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_BSLS, \
+    KC_LCTL,  KC_A,    KC_S,   KC_D,    KC_F,    KC_G,    S(KC_9),   S(KC_0),  KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT, \
+    KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,    MO(SYMB),  MO(META), KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT , \
+    KC_GRV,  ADJUST,  KC_LALT, KC_ESC,  KC_SPC,  KC_LGUI, LOWER,     RAISE,    KC_RGUI, KC_ENT,  KC_SPC,   KC_LBRC, KC_RBRC, KC_BSPC \
   ),
 
   /* META
@@ -43,10 +46,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [META] = LAYOUT( \
-      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, S(KC_4),    S(KC_5), KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
-      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   S(KC_9),    S(KC_0), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_RCBR, KC_PIPE, \
-      _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_LPRN,    KC_RPRN, KC_F12,  KC_F6,   _______, KC_HOME, KC_END,  _______, \
-      _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, KC_VOLU, KC_MPLY, _______ \
+      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      _______, _______,                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
+      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    _______, _______,                 KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
+      _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_F12,  _______, _______, KC_PGDN, KC_PGUP, _______, \
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
   ),
 
   /* SYMB
@@ -61,10 +64,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [SYMB] = LAYOUT( \
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    S(KC_4),    S(KC_5), KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   S(KC_9),    S(KC_0), KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______,    _______, KC_F12,  _______, _______, KC_PGDN, KC_PGUP, _______, \
-    _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, KC_VOLU, KC_MPLY, _______ \
+      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,     _______, _______,              KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
+      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      _______, _______,               KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+      _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_LPRN, KC_RPRN, KC_F12,  KC_F6,  _______, KC_HOME, KC_END,  _______, \
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
   ),
 
   [GAME] =  LAYOUT( \
