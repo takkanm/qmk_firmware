f45f245ec3b9177620fb9e71b7315729974b7c55
takkanm <takkanm@gmail.com>
2018-10-09 17:43:58 +0900

    mod thumb keys

--- keyboards/ergodash/rev2/keymaps/takkanm/keymap.c
+++ keyboards/ergodash/rev2/keymaps/takkanm/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_DOWN,                        KC_UP , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL ,                        KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LOWER ,                        RAISE , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-		KC_GRV, KC_QUOT, ADJUST, KC_LALT,             KC_ESC,   KC_SPC ,KC_LGUI,        KC_RGUI, KC_ENT , KC_SPC,            KC_LCTL, KC_LBRC, KC_RBRC,   ADJUST  \
+		KC_GRV, KC_QUOT, ADJUST, KC_ESC,             KC_SPC,   KC_LGUI ,LOWER,        RAISE, KC_RGUI , KC_ENT,            KC_LALT, KC_LBRC, KC_RBRC,   ADJUST  \
   ),
 
   /* Lower
