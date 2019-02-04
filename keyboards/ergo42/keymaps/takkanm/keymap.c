1d544af7a7e0751421860dae87d9bc2e47298ea3
takkanm <takkanm@gmail.com>
2019-02-04 13:19:59 +0900

    fix keymap

--- keyboards/ergo42/keymaps/takkanm/keymap.c
+++ keyboards/ergo42/keymaps/takkanm/keymap.c
@@ -6,13 +6,18 @@ extern keymap_config_t keymap_config;
 #define META 1
 #define SYMB 2
 #define GAME 3
-#define RAISE MO(META)
-#define LOWER MO(SYMB)
+#define RAISE MO(SYMB)
+#define LOWER MO(META)
 #define ADJUST MO(GAME)
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
+
+#define KC_CTLTB CTL_T(KC_TAB)
+#define KC_CTLESC CTL_T(KC_ESC)
+#define KC_GUIEI GUI_T(KC_LANG2)
+#define KC_ALTKN ALT_T(KC_LANG1)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -28,10 +33,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [BASE] = LAYOUT( \
-    KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,    S(KC_4),   S(KC_5),  KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_BSLS, \
-    KC_LCTL,  KC_A,    KC_S,   KC_D,    KC_F,    KC_G,    S(KC_9),   S(KC_0),  KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT, \
-    KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,    MO(SYMB),  MO(META), KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT , \
-    KC_GRV,  ADJUST,  KC_LALT, KC_ESC,  KC_SPC,  KC_LGUI, LOWER,     RAISE,    KC_RGUI, KC_ENT,  KC_SPC,   KC_LBRC, KC_RBRC, KC_BSPC \
+      KC_TAB,    KC_Q,   KC_W,    KC_E,    KC_R,     KC_T,  S(KC_4),    S(KC_5),  KC_Y,  KC_U,     KC_I,    KC_O,   KC_P,    KC_BSLS, \
+      KC_CTLESC, KC_A,   KC_S,    KC_D,    KC_F,     KC_G,  S(KC_9),    S(KC_0),  KC_H,  KC_J,     KC_K,    KC_L,   KC_SCLN, KC_QUOT, \
+      KC_LSFT,   KC_Z,   KC_X,    KC_C,    KC_V,     KC_B,  MO(SYMB),   MO(META), KC_N,  KC_M,     KC_COMM, KC_DOT, KC_SLSH, KC_RSFT , \
+      KC_GRV,    ADJUST, KC_LALT, XXXXXXX, KC_GUIEI, LOWER, KC_SPC,     KC_ENT,   RAISE, KC_ALTKN, KC_LEFT, KC_UP,  KC_DOWN, KC_RGHT \
   ),
 
   /* META
@@ -49,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      _______, _______,                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    _______, _______,                 KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_F12,  _______, _______, KC_PGDN, KC_PGUP, _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______ \
   ),
 
   /* SYMB
@@ -64,10 +69,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [SYMB] = LAYOUT( \
-      KC_ESC,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,     _______, _______,              KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
-      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      _______, _______,               KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-      _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_LPRN, KC_RPRN, KC_F12,  KC_F6,  _______, KC_HOME, KC_END,  _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
+      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, KC_PIPE, KC_GRV, \
+      _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS,  KC_LBRC, KC_RBRC, KC_BSLS,  KC_TILD, \
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______ \
   ),
 
   [GAME] =  LAYOUT( \
