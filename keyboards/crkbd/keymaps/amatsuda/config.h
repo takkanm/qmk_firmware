795c48929553b3a20ca0f7285da4d72272a58021
Akira Matsuda <ronnie@dio.jp>
2020-07-11 00:08:40 +0900

    _ when ralt was pressed alone

--- keyboards/crkbd/keymaps/amatsuda/config.h
+++ keyboards/crkbd/keymaps/amatsuda/config.h
@@ -35,6 +35,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 100
 
+#define RAPC_KEYS KC_RALT, KC_RSFT, KC_MINS
+
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
     #define RGBLIGHT_ANIMATIONS
