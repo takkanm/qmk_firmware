4d606b66e21074b21433e563047618eee5a1ab70
Akira Matsuda <ronnie@dio.jp>
2020-07-11 11:03:11 +0900

    define PERMISSIVE_HOLD

--- keyboards/crkbd/keymaps/amatsuda/config.h
+++ keyboards/crkbd/keymaps/amatsuda/config.h
@@ -35,7 +35,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 100
 
-// #define PERMISSIVE_HOLD
+#define PERMISSIVE_HOLD
 // #define RETRO_TAPPING
 
 #define RAPC_KEYS KC_RALT, KC_RSFT, KC_MINS
